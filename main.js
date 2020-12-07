285c90759dbc6d7ee8f67ccc9094188f941808bc
wardtooma1 <68230045+wardtooma1@users.noreply.github.com>
2020-12-07 19:23:52 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let fizzBuzz = parseInt(prompt('please type a number'));
+if (fizzBuzz == 3 || fizzBuzz == 6 || fizzBuzz == 9){
+    alert(`${fizzBuzz} is fizz `);
+}else if ( fizzBuzz ==5 ){
+    alert(` ${fizzBuzz} is Buzz`);
+}else {
+    alert( `${fizzBuzz} is nither fiiz or buzz`);
+}
